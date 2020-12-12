0f8631d9372caaa876cde2729304addce01f507a
JohannesBrinz <74264091+JohannesBrinz@users.noreply.github.com>
2020-12-12 14:07:41 +0100

    Update auswertung.py

--- auswertung.py
+++ auswertung.py
@@ -48,24 +48,29 @@ def expo(x, k, y):
 def cdf(x, k, x_0):
     return 1 - np.exp(-k * (x - x_0))
 
+def cdf_umk(y, k, x_0):
+    return
 
 
 #Plots
-#24C runlenght
+#24°C runlenght
 n, bins, patches = plt.hist(C24["Distance[ym]"][0:399], bins = [x0, x0 + size, x0 + 2*size, x0 + 3*size, x0 + 4*size, x0 + 5*size, x0 + 6*size], density = True)
 
 params, params_cov = optimize.curve_fit(expo, bins[0:6], n[0:6])
 
 
-y = expo(np.linspace(0.5, 2.25, 100), params[0], params[1])
+y = expo(np.linspace(0.5, 2, 100), params[0], params[1])
 
-plt.plot(np.linspace(0.5, 2.25, 100)+(size/2), y, "r--", linewidth = 2)           #+size/2 weil ich damit die plot kurve in die mitte der bars packe
-plt.title('Histogram 24C run length', fontsize = 15)
+plt.plot(np.linspace(0.5, 2, 100)+(size/2), y, "r--", linewidth = 2)           #+size/2 weil ich damit die plot kurve in die mitte der bars packe
+plt.title('Histogram 24°C run length', fontsize = 15)
 plt.xlabel('run length [$\mu m$]', fontsize = 13)
-plt.ylabel('probability per runlenght [$\mu m ^{-1}$]', fontsize = 13)
-plt.legend(['fitted pdf, $\mu$ = ' + str(round(params[0],2)),  "measured distribution"], fontsize = 13)
+plt.ylabel('probability distribution [$\mu m ^{-1}$]', fontsize = 13)
+plt.legend(['fitted pdf, $k$ = ' + str(round(params[0],2)),  "measured distribution"], fontsize = 13)
+plt.xticks(ticks=[0.6, 1.0, 1.4, 1.8])
 plt.savefig('Plots/Hist24_run.png', dpi=300)
 plt.clf()
+
+print("F24 =", params[1])
 
 
 #24 C comulative
@@ -77,18 +82,20 @@ p = 1. * np.arange(len(C24["Distance[ym]"][0:399])) / (len(C24["Distance[ym]"][0
 
 
 params, params_cov = optimize.curve_fit(cdf, data_sorted[50:399], p[50:399])    #fit
+perr = np.sqrt(np.diag(params_cov))
 # plot the sorted data:
 plt.plot(data_sorted[50:399], cdf(data_sorted[50:399], params[0], params[1]), c = "black")
 plt.fill_between(data_sorted, p)
 plt.xlabel('runlength [$\mu m$]', fontsize = 13)
-plt.ylabel('cummulative probability per runlength [$\mu m ^{-1}$]', fontsize = 13)
-plt.legend(['fitted cdf, $\mu$ = ' + str(round(params[0],2)),  "measured distribution"], fontsize = 13)
-plt.title('commulative probability 24C run length', fontsize = 15)
+plt.ylabel('cummulative probability distribution [$\mu m ^{-1}$]', fontsize = 13)
+plt.legend(['fitted cdf, $k$ = ' + str(round(params[0],2)),  "measured distribution"], fontsize = 13)
+plt.title('commulative probability 24°C run length', fontsize = 15)
+plt.text(0, 0.9, "$x_0$ = (" + str(round(params[1], 3)) + "$\pm$" + str(round(perr[1], 4)) + ")$\mu m$", fontsize = 13 )
 plt.savefig('Plots/Cum24.png', dpi=300)
 plt.clf()
 
 
-#24C Velocity
+#24°C Velocity
 n, bins, patches = plt.hist(C24["Velocity[ym/s]"][0:399], bins = [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0], density = True)
 
 params, params_cov = optimize.curve_fit(gauss, bins[0:10]+(size/2), n[0:10])
@@ -97,9 +104,9 @@ mu, sigma = norm.fit(C24["Velocity[ym/s]"][0:399])
 y = gauss(np.linspace(0, 1.75, 100), sigma, mu)
 
 plt.plot(np.linspace(0, 1.75, 100), y, "r--", linewidth = 2)
-plt.title('histogram 24C velocity', fontsize = 15)
+plt.title('histogram 24°C velocity', fontsize = 15)
 plt.xlabel('velocity [$\mu m/s$]', fontsize = 13)
-plt.ylabel('probability per velocity [$s/\mu m$]', fontsize = 13)
+plt.ylabel('probability distribution [$s/\mu m$]', fontsize = 13)
 plt.text( 0, 1.5, "$\sigma$ = " + str(round(sigma, 2)), fontsize = 13 )
 plt.axvline(x=mu, ymin=0, ymax=1, c = "black")
 plt.xticks(ticks=[0, 0.4, 0.8, 1.2, 1.6, 2.0])
@@ -110,7 +117,7 @@ plt.clf()
 
 
 
-#34C runlenght
+#34°C runlenght
 x0 = 1.0
 n, bins, patches = plt.hist(C34["Distance[ym]"][0:399], bins = [x0, x0 + size, x0 + 2*size, x0 + 3*size, x0 + 4*size, x0 + 5*size, x0 + 6*size], density = True)
 
@@ -120,12 +127,15 @@ params, params_cov = optimize.curve_fit(expo, bins[0:6], n[0:6])
 y = expo(np.linspace(1, 2.25, 100), params[0], params[1])
 
 plt.plot(np.linspace(1, 2.25, 100)+(size/2), y, "r--", linewidth = 2)
-plt.title('histogram 34C runlength', fontsize = 15)
+plt.title('histogram 34°C runlength', fontsize = 15)
 plt.xlabel('run length [$\mu m$]', fontsize = 13)
 plt.ylabel('probability [$\mu m^{-1}$]', fontsize = 13)
-plt.legend(['fitted pdf, $\mu$ = ' + str(round(params[0],2)),  "measured distribution"], fontsize = 13)
+plt.legend(['fitted pdf, $k$ = ' + str(round(params[0],2)),  "measured distribution"], fontsize = 13)
+plt.xticks(ticks=[1.0, 1.4, 1.8])
 plt.savefig('Plots/Hist34_run.png', dpi=300)
 plt.clf()
+
+print("F34 =", params[1])
 
 #34 C comulative
 # sort the data:
@@ -136,18 +146,21 @@ p = 1. * np.arange(len(C34["Distance[ym]"][0:399])) / (len(C34["Distance[ym]"][0
 
 
 params, params_cov = optimize.curve_fit(cdf, data_sorted[35:399], p[35:399])    #fit
+perr = np.sqrt(np.diag(params_cov))
+
 # plot the sorted data:
 plt.plot(data_sorted[35:399], cdf(data_sorted[35:399], params[0], params[1]), c = "black")
 plt.fill_between(data_sorted, p)
 plt.xlabel('runlength [$\mu m$]', fontsize = 13)
-plt.ylabel('cummulative probability per runlength [$\mu m ^{-1}$]', fontsize = 13)
-plt.title('commulative probability 34C run length', fontsize = 15)
-plt.legend(['fitted cdf, $\mu$ = ' + str(round(params[0],2)),  "measured distribution"], fontsize = 13)
+plt.ylabel('cummulative probability distribution [$\mu m ^{-1}$]', fontsize = 13)
+plt.title('commulative probability 34°C run length', fontsize = 15)
+plt.legend(['fitted cdf, $k$ = ' + str(round(params[0],2)),  "measured distribution"], fontsize = 13)
+plt.text(0, 0.7, "$x_0$ = (" + str(round(params[1], 3)) + "$\pm$" + str(round(perr[1], 4)) + ")$\mu m$", fontsize = 13 )
 plt.savefig('Plots/Cum34.png', dpi=300)
 plt.clf()
 
 
-#34C Velocity
+#34°C Velocity
 n, bins, patches = plt.hist(C34["Velocity[ym/s]"][0:399], bins = [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0], density = True)
 
 params, params_cov = optimize.curve_fit(gauss, bins[0:10]+(size/2), n[0:10])
@@ -156,7 +169,7 @@ mu, sigma = norm.fit(C34["Velocity[ym/s]"][0:399])
 y = gauss(np.linspace(0.4, 2.4, 100), sigma, mu)
 
 plt.plot(np.linspace(0.4, 2.4, 100), y, "r--", linewidth = 2)
-plt.title('histogram 34C velocity', fontsize = 15)
+plt.title('histogram 34°C velocity', fontsize = 15)
 plt.xlabel('velocity [$\mu m/s$]', fontsize = 13)
 plt.ylabel('probability [$s/\mu m$]', fontsize = 13)
 plt.text( 0, 1.75, "$\sigma$ = " + str(round(sigma, 2)), fontsize = 13 )
